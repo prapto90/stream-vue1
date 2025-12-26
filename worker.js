daefefa7634ed0a299566a3a9ac2e513231d35ae
prapto90 <43087829+prapto90@users.noreply.github.com>
2025-12-27 02:32:24 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,9 @@
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      return env.ASSETS.fetch(new Request(url.origin + "/index.html"));
+    }
     return env.ASSETS.fetch(request);
   }
 };
