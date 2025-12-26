cf6a8485ebc8801ff42991d02e2043288d3a23bb
prapto90 <43087829+prapto90@users.noreply.github.com>
2025-12-27 02:24:43 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,19 +1,5 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-
-// Mendaftarkan event handler untuk menangani permintaan 'fetch' (permintaan HTTP)
-addEventListener("fetch", (event) => {
-  event.respondWith(handleEvent(event));
-});
-
-async function handleEvent(event) {
-  try {
-    // Logika Workers Site: Mencoba mengambil aset statis dari KV
-    return await getAssetFromKV(event);
-  } catch (e) {
-    // Jika tidak ditemukan, kembalikan 404
-    let pathname = new URL(event.request.url).pathname;
-    return new Response(`"${pathname}" not found`, {
-      status: 404,
-    });
+export default {
+  async fetch(request, env, ctx) {
+    return env.ASSETS.fetch(request);
   }
-}
+};
