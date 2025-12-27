cfc2bfa929e1ef6107e60ecc6b4cf25dd2e09fdd
prapto90 <43087829+prapto90@users.noreply.github.com>
2025-12-27 12:15:35 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,13 +2,9 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    // ===============================
-    // 1. API PROXY KE VPS PYTHON
-    // ===============================
-    if (url.pathname.startsWith("/api/")) {
-      const target =
-        env.API_BASE_URL + url.pathname.replace("/api", "");
-        console.log("API:", env.API_BASE_URL);
+    // ===== API PROXY =====
+    if (url.pathname.startsWith("/auth/")) {
+      const target = env.API_BASE_URL + url.pathname;
 
       return fetch(target, {
         method: request.method,
@@ -17,24 +13,14 @@ export default {
       });
     }
 
-    // ===============================
-    // 2. ROOT → index.html
-    // ===============================
+    // ===== SPA =====
     if (url.pathname === "/") {
       return env.ASSETS.fetch(new Request("/index.html", request));
     }
 
-    // ===============================
-    // 3. FILE STATIS
-    // ===============================
     const asset = await env.ASSETS.fetch(request);
-    if (asset.status !== 404) {
-      return asset;
-    }
+    if (asset.status !== 404) return asset;
 
-    // ===============================
-    // 4. SPA FALLBACK (Vue Router)
-    // ===============================
     return env.ASSETS.fetch(new Request("/index.html", request));
   }
 };
