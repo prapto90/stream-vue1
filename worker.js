48141c7e420975a345eab92465b6805a5f056f18
prapto90 <43087829+prapto90@users.noreply.github.com>
2025-12-27 12:25:33 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,18 +2,18 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    // ===== API PROXY =====
     if (url.pathname.startsWith("/auth/")) {
       const target = env.API_BASE_URL + url.pathname;
+      const headers = new Headers(request.headers);
+      headers.delete("content-length");
 
       return fetch(target, {
         method: request.method,
-        headers: request.headers,
+        headers,
         body: request.method !== "GET" ? request.body : null,
       });
     }
 
-    // ===== SPA =====
     if (url.pathname === "/") {
       return env.ASSETS.fetch(new Request("/index.html", request));
     }
