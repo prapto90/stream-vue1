718f1ddf6dc82ea1ef3bdf32ca1550d1791d4506
prapto90 <43087829+prapto90@users.noreply.github.com>
2025-12-27 12:42:40 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,18 +1,7 @@
-export default {
-  async fetch(request, env) {
-    try {
-      return new Response(
-        JSON.stringify({
-          api_base: env.API_BASE_URL,
-          path: new URL(request.url).pathname
-        }),
-        { headers: { "Content-Type": "application/json" } }
-      );
-    } catch (e) {
-      return new Response(
-        JSON.stringify({ error: e.message }),
-        { status: 500 }
-      );
-    }
-  }
-};
+return new Response(
+  JSON.stringify({
+    api_base: env.API_BASE_URL,
+    path: new URL(request.url).pathname
+  }),
+  { headers: { "Content-Type": "application/json" } }
+);
